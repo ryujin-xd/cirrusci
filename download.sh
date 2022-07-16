16944b6d5481b5bed6e6e24c84a398dae5351a5e
ryujin-xd <ryujinxd6@gmail.com>
2022-07-17 04:04:42 +0700

    .

--- download.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# Kernel Sources
-git config --global user.email "ryujinxd6@gmail.com" -y && git config --global user.name "ryujin-xd"
-git clone --depth=1 https://github.com/ryujin-xd/android_kernel_xiaomi_ginkgo-1 -b Q $CIRRUS_WORKING_DIR/KERNEL
-
-git clone --depth=1 https://github.com/kdrag0n/proton-clang $CIRRUS_WORKING_DIR/CLANG
--- proton-compile.sh
+++ proton-compile.sh
@@ -1,22 +1,24 @@
 #!/usr/bin/env bash
-  
+ 
 # Main Declaration
 function env() {
-export KERNEL_NAME=KERNEL
-KERNEL_ROOTDIR=$CIRRUS_WORKING_DIR/KERNEL
-DEVICE_DEFCONFIG=vendor/juice_defconfig
-CLANG_ROOTDIR=$CIRRUS_WORKING_DIR/CLANG
-CLANG_VER="$("$CLANG_ROOTDIR"/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')"
-LLD_VER="$("$CLANG_ROOTDIR"/bin/ld.lld --version | head -n 1)"
-IMGS=$KERNEL_ROOTDIR/out/arch/arm64/boot/Image.gz-dtb
-DATE=$(date +"%F-%S")
-START=$(date +"%s")
+export KERNEL_NAME=PERF-PROTON
 export KBUILD_BUILD_USER=$BUILD_USER
 export KBUILD_BUILD_HOST=$BUILD_HOST
 export KBUILD_COMPILER_STRING="$CLANG_VER with $LLD_VER"
-export BOT_MSG_URL="https://api.telegram.org/$TG_TOKEN/sendMessage"
-export BOT_MSG_URL2="https://api.telegram.org/$TG_TOKEN"
+
+KERNEL_ROOTDIR=$CIRRUS_WORKING_DIR/KERNEL
+DEVICE_DEFCONFIG=vendor/ginkgo-perf_defconfig
+IMGS=$KERNEL_ROOTDIR/out/arch/arm64/boot/Image
+
+CLANG_ROOTDIR=$CIRRUS_WORKING_DIR/PROTON-CLANG
+CLANG_VER="$("$CLANG_ROOTDIR"/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')"
+LLD_VER="$("$CLANG_ROOTDIR"/bin/ld.lld --version | head -n 1)"
+
+DATE=$(date +"%F-%S")
+START=$(date +"%s")
 }
+
 # Checking environtment
 # Warning !! Dont Change anything there without known reason.
 function check() {
@@ -29,31 +31,27 @@ echo CLANG_ROOTDIR = ${CLANG_ROOTDIR}
 echo KERNEL_ROOTDIR = ${KERNEL_ROOTDIR}
 echo ================================================
 }
-tg_post_msg() {
-  curl -s -X POST "$BOT_MSG_URL" -d chat_id="$TG_CHAT_ID" \
-  -d "disable_web_page_preview=true" \
-  -d "parse_mode=html" \
-  -d text="$1"
-}
+
 # Compile
 compile(){
 cd ${KERNEL_ROOTDIR}
-    make -j$(nproc) O=out ARCH=arm64 ${DEVICE_DEFCONFIG}
-    make -j$(nproc) ARCH=arm64 O=out \
+    make -j$(nproc) O=out ARCH=arm64 SUBARCH=arm64 ${DEVICE_DEFCONFIG}
+    make -j$(nproc) ARCH=arm64 SUBARCH=arm64 O=out \
     CC=${CLANG_ROOTDIR}/bin/clang \
+    LD=${CLANG_ROOTDIR}/bin/ld.lld \
     CROSS_COMPILE=${CLANG_ROOTDIR}/bin/aarch64-linux-gnu- \
     CROSS_COMPILE_ARM32=${CLANG_ROOTDIR}/bin/arm-linux-gnueabi-
-
    if ! [ -a "$IMGS" ]; then
 	finerr
    fi
-   git clone --depth=1 https://github.com/fadlyas07/anykernel-3 $CIRRUS_WORKING_DIR/AnyKernel
+   git clone --depth=1 https://github.com/ZenitsuID/AnyKernel3 -b lavender $CIRRUS_WORKING_DIR/AnyKernel
    cp $IMGS $CIRRUS_WORKING_DIR/AnyKernel
 }
+
 # Push kernel to channel
 function push() {
     cd $CIRRUS_WORKING_DIR/AnyKernel
-    zip -r9 $KERNEL_NAME-$DEVICE_CODENAME-${DATE}.zip *
+    zip -r9 $KERNEL_NAME-${DATE}.zip *
     ZIP=$(echo *.zip)
     curl -F document=@$ZIP "https://api.telegram.org/$TG_TOKEN/sendDocument" \
         -F chat_id="$TG_CHAT_ID" \
@@ -61,7 +59,6 @@ function push() {
         -F "parse_mode=html" \
         -F caption="$KERNEL_NAME
 ==========================
-👤 Owner: ryujin-xd
 🏚️ Linux version: $KERNEL_VERSION
 🌿 Branch: $BRANCH
 🎁 Top commit: $LATEST_COMMIT
@@ -71,18 +68,20 @@ function push() {
 ==========================
 Compile took $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) second(s)."
 }
+
 # Fin Error
 function finerr() {
-    curl -s -X POST "$BOT_MSG_URL" -d chat_id="$TG_CHAT_ID" \
+    curl -s -X POST "https://api.telegram.org/$TG_TOKEN/sendMessage" -d chat_id="$TG_CHAT_ID" \
 	    -d "disable_web_page_preview=true" \
 	    -d "parse_mode=html" \
-        -d text="==============================%0A<b>    Building Kernel CLANG Failed [❌]</b>%0A==============================" \
-    curl -s -X POST "$BOT_MSG_URL2/sendSticker" \
+        -d text="==============================%0A<b>    Building Kernel PROTON-CLANG Failed [❌]</b>%0A==============================" \
+    curl -s -X POST "https://api.telegram.org/$TG_TOKEN/sendSticker" \
         -d sticker="CAACAgIAAx0CXjGT1gACDRRhYsUKSwZJQFzmR6eKz2aP30iKqQACPgADr8ZRGiaKo_SrpcJQIQQ" \
         -d chat_id="$TG_CHAT_ID"
     exit 1
 }
 
+# Info
 function info() {
 cd $KERNEL_ROOTDIR
 KERNEL_VERSION=$(cat $KERNEL_ROOTDIR/out/.config | grep Linux/arm64 | cut -d " " -f3)
@@ -92,7 +91,6 @@ TRIGGER_SHA="$(git rev-parse HEAD)"
 LATEST_COMMIT="$(git log --pretty=format:'%s' -1)"
 COMMIT_BY="$(git log --pretty=format:'by %an' -1)"
 BRANCH="$(git rev-parse --abbrev-ref HEAD)"
-
 }
 
 env
@@ -102,3 +100,4 @@ END=$(date +"%s")
 DIFF=$(($END - $START))
 info
 push
+ 
