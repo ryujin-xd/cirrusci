177e02e79ad1776503d7d0d8c26295f4971acbe8
ryujin-xd <ryujinxd6@gmail.com>
2022-07-17 04:18:05 +0700

    .

--- sync.sh
+++ sync.sh
@@ -7,5 +7,5 @@ git clone --depth=1 https://github.com/ryujin-xd/android_kernel_xiaomi_ginkgo-2 
 # Proton Clang ---
 git clone --depth=1 https://github.com/kdrag0n/proton-clang $CIRRUS_WORKING_DIR/PROTON-CLANG
 # Azure Clang ---
-git clone --depth=1 https://gitlab.com/Panchajanya1999/azure-clang $CIRRUS_WORKING_DIR/AZURE-CLANG
+#git clone --depth=1 https://gitlab.com/Panchajanya1999/azure-clang $CIRRUS_WORKING_DIR/AZURE-CLANG
  
